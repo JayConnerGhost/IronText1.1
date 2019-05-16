894473a53edcf2b4f81f77f202311d04251678ac
jay katie martin <ghosttk@protonmail.com>
2019-05-16 12:23:22 +0100

    initial Docking implementation

---
+++ NotesPad/NotesPad/Container.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace NotesPad
+{
+    partial class Container
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.vS2015DarkTheme1 = new WeifenLuo.WinFormsUI.Docking.VS2015DarkTheme();
+            this.dockPanel = new WeifenLuo.WinFormsUI.Docking.DockPanel();
+            this.SuspendLayout();
+            // 
+            // dockPanel
+            // 
+            this.dockPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dockPanel.Location = new System.Drawing.Point(0, 0);
+            this.dockPanel.Name = "dockPanel";
+            this.dockPanel.Size = new System.Drawing.Size(800, 450);
+            this.dockPanel.TabIndex = 0;
+            // 
+            // Container
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dockPanel);
+            this.IsMdiContainer = true;
+            this.Name = "Container";
+            this.Text = "IronText 1.1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private WeifenLuo.WinFormsUI.Docking.VS2015DarkTheme vS2015DarkTheme1;
+        private WeifenLuo.WinFormsUI.Docking.DockPanel dockPanel;
+    }
+}
+

6da3e4c281cb0fb0b0ce25e72cc7c2e42d44bb13
jay katie martin <ghosttk@protonmail.com>
2019-05-25 23:39:17 +0100

    Added basic object database https://github.com/mbdavid/LiteDB/wiki/Getting-Started

--- NotesPad/NotesPad.Data/IdeaRepository.cs
+++ NotesPad/NotesPad.Data/IdeaRepository.cs
@@ -1,18 +1,38 @@
 ﻿using System;
+using System.Configuration;
+using System.Linq;
+using System.Runtime.InteropServices;
+using LiteDB;
 using NotesPad.Objects;
 
 namespace NotesPad.Data
 {
     public class IdeaRepository : IIdeaRepository
     {
+        private readonly LiteDatabase _database;
+        private string _connectionString="";
+        private LiteCollection<Idea> _ideas;
+
+        public  IdeaRepository()
+        {
+            var appSettingsReader =new System.Configuration.AppSettingsReader();
+            _connectionString = (string)appSettingsReader.GetValue("ConnectionString", typeof(string));
+            _database = new LiteDatabase(_connectionString);
+         
+            _ideas = _database.GetCollection<Idea>("Ideas");
+            _ideas.EnsureIndex("Name");
+        }
         public void Save(IIdea idea)
         {
-            throw new NotImplementedException();
+            var ideaToStore = (Idea)idea;
+            ideaToStore._id=Guid.NewGuid();
+            _ideas.Insert(ideaToStore);
         }
 
         public IIdea FindbyName(string testIdea)
         {
-            throw new NotImplementedException();
+            var results = _ideas.Find(x => x.Name==testIdea);
+            return results.ToList().FirstOrDefault();
         }
     }
 }
--- NotesPad/NotesPad.Objects/Idea.cs
+++ NotesPad/NotesPad.Objects/Idea.cs
@@ -1,7 +1,10 @@
-﻿namespace NotesPad.Objects
+﻿using System;
+
+namespace NotesPad.Objects
 {
     public class Idea : IIdea
     {
+        public Guid _id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
     }

62c7c46672750c28cf81e867d8b645e97c08f565
jay katie martin <jaykatiemartin@outlook.com>
2019-05-25 11:45:02 +0100

    adding skeliton for persistance layer

---
+++ NotesPad/NotesPad.Data.Tests/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesPad.Data.Tests
+{
+    public class Class1
+    {
+    }
+}

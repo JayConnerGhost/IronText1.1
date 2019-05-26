1b5bb441bcd750c3ab6af489e38a72ab86962836
jay katie martin <ghosttk@protonmail.com>
2019-05-26 11:08:35 +0100

    adding simple test to drive out add on ideaservice

---
+++ NotesPad/NotesPad.Services.Tests/IdeaSpecifications.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NotesPad.Data;
+using NotesPad.Objects;
+using NSubstitute;
+using Xunit;
+
+namespace NotesPad.Services.Tests
+{
+    public class IdeaSpecifications
+    {
+        [Fact]
+        public void Repository_is_called_add_idea_is_called_from_service()
+        {
+            //Arrange 
+            var idea = new Idea {Name = "test idea", Description = "test idea description"};
+            IIdeaRepository respository=NSubstitute.Substitute.For<IIdeaRepository>();
+            //Act
+            IIdeaService service=new IdeaService();
+            service.Add(idea);
+
+            //Assert
+            respository.Received().Save(idea);
+        }
+    }
+}
--- NotesPad/NotesPad.Services/IdeaService.cs
+++ NotesPad/NotesPad.Services/IdeaService.cs
@@ -1,13 +1,20 @@
 ﻿using System;
+using NotesPad.Data;
 using NotesPad.Objects;
 
 namespace NotesPad.Services
 {
     public class IdeaService : IIdeaService
     {
+        private readonly IIdeaRepository _repository;
+
+        public IdeaService(IIdeaRepository repository)
+        {
+            _repository = repository;
+        }
         public void Add(Idea idea)
         {
-            throw new NotImplementedException();
+            _repository.Save(idea);
         }
     }
 }
